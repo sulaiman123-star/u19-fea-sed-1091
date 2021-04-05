7cef9efe916d12ec831a45dbe2cca4cc29c86d43
sulaiman123-star <81955280+sulaiman123-star@users.noreply.github.com>
2021-04-05 09:14:05 +0100

    Add files via upload

---
+++ ridwan.py
@@ -0,0 +1,26 @@
+print('Question ONE')
+NAME="SULAIMAN RIDWAN"
+DEPARTMENT=' SCIENCE EDUCATION DEPARTMENT'
+print('my name is',NAME,'i am from',DEPARTMENT)
+print('Question TWO')
+love='Boo!'
+print(love)
+print('Question THREE')
+Numb1=4
+Numb2=6
+Numb3=Numb1+Numb2
+print(Numb1,Numb2,Numb3)
+print('Question FOUR')
+clear=12/0.5
+print(clear)
+print('Question FIVE')
+Adding=5.1 + 6
+print(Adding)
+print('Question SIX')
+sure=8%3
+print(sure)
+print('Question SEVEN')
+Result1=2 * 4
+Result2=4 + 2
+Result3=Result1 * Result2
+print(Result3)
