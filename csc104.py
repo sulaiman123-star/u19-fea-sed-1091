58ce19dcc63c307d41e7a88dbf6eda207bc431ad
sulaiman123-star <81955280+sulaiman123-star@users.noreply.github.com>
2021-05-28 22:24:06 +0100

    Add files via upload

---
+++ csc104.py
@@ -0,0 +1,86 @@
+USSD_CODE = ("GTBANK : *737#")
+print(USSD_CODE)
+list = [ "\n1.Airtime-self",  "2.Airtime-others",  "3.Data", "4.Trasf-GTB", "5.Transfer-others", "6.Transfer-MFB/ewallet", "7.Cable Tv", "8.Pay bills", "9.Next ", "0> to terminate"]
+print(list[0])
+print (list[1])
+print(list[2])
+print (list[3])
+print(list[4])
+print(list[9])
+option_1= '\nPlease select an option: 1'
+print(option_1)
+display_76= '\nEnter the amount : 100'
+print(display_76)
+display_41="\nplease wait your request has been process"
+print(display_41)
+option_0=   "\nENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "\nENTER 2 TO END"
+print(option_05)
+display_4 = "\nEnter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list[4])
+print(list[9])
+option_2 = '\nPlease select an option: 2'
+print (option_2)
+display_7= '\nAirtime-others'
+print(display_7)
+display_6='\nplease enter the 3rd party number : 08075109989'
+print(display_6)
+display_3= "\nplease enter the amount : 300"
+print(display_3)
+display_4= "\nEnter your 737 PIN or Token Code to Top Up 08075109989 on Glo with NGN 100 or Enter 0 to Cancel"
+display_32="\nplease wait your request has been process"
+print(display_32)
+display_4 = "\nEnter 1 or 2 : 1"
+print(display_4)
+option_0=  "\nENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "\nENTER 2 TO END"
+print(option_05)
+display_4 = "\nEnter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list[4])
+print(list[9])
+option_3= "Please select an option: 3"
+print(option_3)
+display_56= "\npurchase data for"
+print(display_56)
+list1= ["\n1.self", "2. 3rd party"]
+print( list1[0])
+print( list1[1])
+display_4 = "Enter 1 or 2 : 2"
+print(display_4)
+display_6= 'Enter Amount : 200'
+print(display_6)
+display_7= 'Enter  number you want to buy airtime for: 09039384939'
+print(display_7)
+display_4= "Do you want to buy 200 naira worth of airtime for 09039384939?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05) 
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list[9])
+display_4 = "Enter 1 or 2 : 0"
+print(display_4)
+print("\n \n Thank you for choosing GTBANK \n We appreciate you Always")
